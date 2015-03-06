b386a919bbf43a38a5f10e604a50e6f8ddb196ba
Thomas Klose <thomas@bratler.net>
2015-03-06 20:12:20 +0100

    Firmware Grinder renamed

--- InGame Programming/InGame Scripts/FirmwareGrinder.cs
+++ InGame Programming/InGame Scripts/FirmwareGrinder.cs
@@ -12,7 +12,7 @@ using VRage;
 
 namespace BaconfistSEInGameScript
 {
-    class FirmwareHexler
+    class FirmwareGrinder
     {
         IMyGridTerminalSystem GridTerminalSystem;
         String Storage;
