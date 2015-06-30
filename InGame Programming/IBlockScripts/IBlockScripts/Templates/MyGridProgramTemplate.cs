23d5202be3c1259c8598bee36f0db91a9396c9ec
Thomas Klose <thomas@bratler.net>
2015-06-30 02:10:39 +0200

    Update to new script base
    removen/archived old scripts
    updated with new Script-base

---
+++ InGame Programming/IBlockScripts/IBlockScripts/Templates/MyGridProgramTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Common.ObjectBuilders;
+using VRage;
+using VRageMath;
+
+namespace IBlockScripts
+{
+    public class $safeitemname$ : MyGridProgram
+    {
+        #region Game Code - Copy/Paste Code from this region into Block Script Window in Game
+        void Main(string args)
+        {
+           
+        }
+        #endregion
+    }
+}
