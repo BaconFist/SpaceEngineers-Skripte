d74343dc1a99073ea92b5477de31804d322c5ff5
Thomas Klose <thomas@bratler.net>
2015-07-04 02:49:17 +0200

    new Simple Debuger
    Uses ProgrammableBlock Terminal to schow debug info.
    To be included in other scripts

---
+++ InGame Programming/IBlockScripts/IBlockScripts/Classes/MyDebugger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Common.ObjectBuilders;
+using VRage;
+using VRageMath;
+
+namespace IBlockScripts
+{
+    public class MyDebugger : MyGridProgram
+    {
+        #region Game Code - Copy/Paste Code from this region into Block Script Window in Game
+        /**
+           MyDebugger
+           ==============================
+           Copyright (c) 2015 Thomas Klose <thomas@bratler.net>
+           Source:  
+           
+           Summary
+           ------------------------------
+           
+
+           Abstract
+           ------------------------------
+          
+           
+           Example
+           ------------------------------
+          
+       */
+        void Main(string args)
+        {
+            MyDebug Debug = new MyDebug(this);
+            Debug.write("Copy the MyDebug Clacc to your script.");
+        }
+
+        #region copy from here
+        class MyDebug
+        {
+            MyGridProgram program;
+            public MyDebug(MyGridProgram program)
+            {
+                this.program = program;
+            }
+
+            public void write(string msg)
+            {
+                string arg = "[" + program.ElapsedTime.TotalMilliseconds.ToString() + "] " + msg;
+                program.Echo(arg);
+            }
+        }
+        #endregion copy to here
+        #endregion
+    }
+}
