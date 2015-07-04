9be9d4d19d524b4a3067b4b238cd2132c680c52a
Thomas Klose <thomas@bratler.net>
2015-07-04 02:53:55 +0200

    added information

--- InGame Programming/IBlockScripts/IBlockScripts/Classes/MyDebugger.cs
+++ InGame Programming/IBlockScripts/IBlockScripts/Classes/MyDebugger.cs
@@ -19,19 +19,21 @@ namespace IBlockScripts
            MyDebugger
            ==============================
            Copyright (c) 2015 Thomas Klose <thomas@bratler.net>
-           Source:  
+           Source: https://github.com/BaconFist/SpaceEngineers-Skripte/blob/master/InGame%20Programming/IBlockScripts/IBlockScripts/Classes/MyDebugger.cs 
            
            Summary
            ------------------------------
-           
+           Library to be included in Scripts to Show debuginfo in Terminal
 
            Abstract
            ------------------------------
-          
+           Copy the MyDebug Class to your script.
+           call Debug.write("debug message") to add a message to Terminal info.
+           it will be formatted with milliseconds elapsed.
            
            Example
            ------------------------------
-          
+           see Mainmethod at source.
        */
         void Main(string args)
         {
