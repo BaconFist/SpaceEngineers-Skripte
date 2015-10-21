abd53facb56ead59343cbcb9ed7f6363f0264795
Thomas Klose <thomas@bratler.net>
2015-10-21 05:20:41 +0200

    Update: MassBlockRenamer
    Added Commands to add/delete from blocknames

--- InGame Programming/IBlockScripts/IBlockScripts/Controller/MassBlockRenamer.cs
+++ InGame Programming/IBlockScripts/IBlockScripts/Controller/MassBlockRenamer.cs
@@ -29,6 +29,12 @@ namespace IBlockScripts
            ------------------------------
             use argument for new Text and Text to replace splittet by ;
 
+            Commands:
+             * Commands are used instead oldText like "THECOMMAND;newText"
+                CMD:ADD => adds newText to all blocks without replacing (adding ship id/name to all blocks)
+                            Example:  "CMD:ADD; [My Awesome Ship]"
+                CMD:DEL => delete newText 
+                            Exapmle: "CMD:DEL;weird text to be removed"
            
            Example
            ------------------------------
@@ -36,24 +42,69 @@ namespace IBlockScripts
             
           
        */
+
+        const string CMD_ADD = "CMD:ADD";
+        const string CMD_DEL = "CMD:DEL";
+
         void Main(string args)
         {
             string[] argv = args.Split(';');
-            if(argv.Length == 2)
+            if (argv.Length == 2)
             {
                 string oldString = argv[0];
                 string newString = argv[1];
 
                 List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
-                GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks, (x => (x as IMyTerminalBlock).CustomName.Contains(oldString)));
-                
-                for(int i = 0; i < blocks.Count; i++)
+                if (oldString.Equals(CMD_ADD))
                 {
-                    (blocks[i] as IMyTerminalBlock).SetCustomName((blocks[i] as IMyTerminalBlock).CustomName.Replace(oldString,newString));
-                }
-                
+                    Echo("ADD");
+                    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks);
+                    doAdd(blocks, newString);
+                } else if (oldString.Equals(CMD_DEL))
+                {
+                    Echo("DEL");
+                    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks);
+                    doDel(blocks, newString);
+                } else
+                {
+                    Echo("default");
+                    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(blocks, (x => (x as IMyTerminalBlock).CustomName.Contains(oldString)));
+                    doReplace(blocks, oldString, newString);
+                }               
             }
         }
+
+        void doAdd(List<IMyTerminalBlock> blocks, string newText)
+        {
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                IMyTerminalBlock block = (blocks[i] as IMyTerminalBlock);
+                string oldName = block.CustomName;
+                block.SetCustomName(oldName + newText);
+            }
+        }
+
+        void doDel(List<IMyTerminalBlock> blocks, string newText)
+        {
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                IMyTerminalBlock block = (blocks[i] as IMyTerminalBlock);
+                string oldName = block.CustomName;
+                block.SetCustomName(oldName.Replace(newText, ""));
+            }
+        }
+
+
+        void doReplace(List<IMyTerminalBlock> blocks, string oldText, string newText)
+        {
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                IMyTerminalBlock block = (blocks[i] as IMyTerminalBlock);
+                string oldName = block.CustomName;
+                block.SetCustomName(oldName.Replace(oldText, newText));
+            }
+        }
+
         #endregion
     }
 }
