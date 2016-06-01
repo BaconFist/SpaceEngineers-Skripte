48a09c953c95b33e31d4161f6766d325677dbd06
Thomas Klose <thomas@bratler.net>
2016-06-01 15:05:43 +0200

    new script template

--- InGame Programming/IBlockScripts/IBlockScripts/Templates/MyGridProgramTemplate.cs
+++ InGame Programming/IBlockScripts/IBlockScripts/Templates/MyGridProgramTemplate.cs
@@ -7,20 +7,67 @@ using System.Threading.Tasks;
 using Sandbox.ModAPI;
 using Sandbox.ModAPI.Ingame;
 using Sandbox.ModAPI.Interfaces;
-//using Sandbox.Common.ObjectBuilders;
 using VRage;
 using VRageMath;
 
 namespace IBlockScripts
 {
-    public class safeitemname : MyGridProgram
-    //        public class $safeitemname$ : MyGridProgram
+    public class $safeitemname$ : MyGridProgram
     {
         #region Game Code - Copy/Paste Code from this region into Block Script Window in Game
-        void Main(string args)
-        {
+        /**
+           $safeitemname$
+           ==============================
+           Copyright (c) 2016 Thomas Klose <thomas@bratler.net>
+           Source:  
            
+           Summary
+           ------------------------------
+           
+           Abstract
+           ------------------------------
+                       
+           Example
+           ------------------------------          
+          
+       */
+
+        public $safeitemname$()
+        {
+
+            // The constructor, called only once every session and
+            // always before any other method is called. Use it to
+            // initialize your script. 
+            //     
+            // The constructor is optional and can be removed if not
+            // needed.
+
         }
-        #endregion
+
+
+
+        public void Save()
+        {
+
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+
+        }
+
+
+
+        public void Main(string argument)
+        {
+            // The main entry point of the script, invoked every time
+            // one of the programmable block's Run actions are invoked.
+            // 
+            // The method itself is required, but the argument above
+            // can be removed if not needed.
+        }
+        #endregion End of  Game Code - Copy/Paste Code from this region into Block Script Window in Game
     }
 }
