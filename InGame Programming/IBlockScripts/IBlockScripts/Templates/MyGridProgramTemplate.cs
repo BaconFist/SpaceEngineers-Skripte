ec1affdb0eb768eb80becc9cfa05d18e4189f6dc
Thomas Klose <thomas@bratler.net>
2016-06-06 23:59:58 +0200

    updated template and references

--- InGame Programming/IBlockScripts/IBlockScripts/Templates/MyGridProgramTemplate.cs
+++ InGame Programming/IBlockScripts/IBlockScripts/Templates/MyGridProgramTemplate.cs
@@ -1,73 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Sandbox.ModAPI;
-using Sandbox.ModAPI.Ingame;
-using Sandbox.ModAPI.Interfaces;
-using VRage;
 using VRageMath;
+using VRage.Game;
+using System.Text;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Collections;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
 
-namespace IBlockScripts
+public class Program : MyGridProgram
 {
-    public class $safeitemname$ : MyGridProgram
+    #region Game Code - Copy/Paste Code from this region into Block Script Window in Game
+    /**
+	   $safeitemname$
+	   ==============================
+	   Copyright (c) 2016 Thomas Klose <thomas@bratler.net>
+	   Source:  
+	   
+	   Summary
+	   ------------------------------
+	   
+	   Abstract
+	   ------------------------------
+				   
+	   Example
+	   ------------------------------          
+	  
+   */
+
+    public Program()
     {
-        #region Game Code - Copy/Paste Code from this region into Block Script Window in Game
-        /**
-           $safeitemname$
-           ==============================
-           Copyright (c) 2016 Thomas Klose <thomas@bratler.net>
-           Source:  
-           
-           Summary
-           ------------------------------
-           
-           Abstract
-           ------------------------------
-                       
-           Example
-           ------------------------------          
-          
-       */
 
-        public $safeitemname$()
-        {
+        // The constructor, called only once every session and
+        // always before any other method is called. Use it to
+        // initialize your script. 
+        //     
+        // The constructor is optional and can be removed if not
+        // needed.
 
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-
-        }
-
-
-
-        public void Save()
-        {
-
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
-
-        }
-
-
-
-        public void Main(string argument)
-        {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked.
-            // 
-            // The method itself is required, but the argument above
-            // can be removed if not needed.
-        }
-        #endregion End of  Game Code - Copy/Paste Code from this region into Block Script Window in Game
     }
+
+    public void Save()
+    {
+
+        // Called when the program needs to save its state. Use
+        // this method to save your state to the Storage field
+        // or some other means. 
+        // 
+        // This method is optional and can be removed if not
+        // needed.
+
+    }
+
+    public void Main(string argument)
+    {
+        // The main entry point of the script, invoked every time
+        // one of the programmable block's Run actions are invoked.
+        // 
+        // The method itself is required, but the argument above
+        // can be removed if not needed.
+    }
+    #endregion End of  Game Code - Copy/Paste Code to this region into Block Script Window in Game
 }
